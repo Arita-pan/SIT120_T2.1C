2ecf882b184c747c2e2d7294e88c4ad59f728053
Arita-pan <124896635+Arita-pan@users.noreply.github.com>
2024-09-27 02:15:09 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    host: true, // Allow external access
+        port: 5173, // Use the desired port
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
